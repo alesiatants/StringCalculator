f2857b39e3dcf450e598ef89aff7203b62c0fa11
Alesia <140408589+alesiatants@users.noreply.github.com>
2024-10-10 13:08:58 +0300

    Update calculation.go

--- Калькулятор/calculation.go
+++ Калькулятор/calculation.go
@@ -1,39 +1,78 @@
 package main
 
-import (
-	"fmt"
+import(
 	"strconv"
-	"strings"
+	"fmt"
 )
 
-func EvalMathExpr(expr string) (float64, error) {
-	//применение функции валидации строки выражения
-	if !IsValidExpression(expr) {
-		return 0, fmt.Errorf("Некорректное выражение")
+func precedence_operation(op string) int {
+	//описание приоритета выполнения операций
+	switch op {
+	case "+", "-":
+	 return 1
+	case "*", "/":
+	 return 2
+	default:
+	 return 0
 	}
-	// Оценка выражения в скобках
-	// Удаление всех пробелов из выражения
-	expr = strings.ReplaceAll(expr, " ", "")
-
-	// Оценка выражения в скобках
-	for {
-		start := strings.Index(expr, "(")
-		if start == -1 {
-			break
-		}
-		end := strings.Index(expr, ")")
-		if end == -1 {
-			return 0, fmt.Errorf("некорректное выражение")
-		}
-		subExpr := expr[start+1 : end]
-		// Используем цикл for вместо рекурсии для оценки подвыражений
-		subResult, err := evalSubExpr(subExpr)
-		if err != nil {
-			return 0, err
-		}
-
-		expr = expr[:start] + strconv.FormatFloat(subResult, 'f', 6, 64) + expr[end+1:]
-	}
-return evalSubExpr(expr)
-
-}
+ }
+ 
+ func calculate(node *TreeNode) (float64, error) {
+	 if node == nil {
+			 return 0, fmt.Errorf("Дерево не заполнено")
+	 }
+	 stack := []*TreeNode{}
+	 var curr *TreeNode
+	 values := map[*TreeNode]float64{}
+	 visited := map[*TreeNode]bool{}
+ 
+	 stack = append(stack, node)
+ 
+	 for len(stack) > 0 {
+			 curr = stack[len(stack)-1]
+			 // Если это листовой узел (число)
+			 if curr.Left == nil && curr.Right == nil {
+					 val, err := strconv.ParseFloat(curr.Value, 64)
+					 if err!=nil{
+						return 0,err
+					 }
+					 values[curr] = val
+					 stack = stack[:len(stack)-1]
+					 continue
+			 }
+ 
+			 if visited[curr] {
+					 leftVal := values[curr.Left]
+					 rightVal := values[curr.Right]
+					 var result float64
+					 //производим вычисления операции-узла над его потомками
+ 
+					 switch curr.Value {
+					 case "+":
+							 result = leftVal + rightVal
+					 case "-":
+							 result = leftVal - rightVal
+					 case "*":
+							 result = leftVal * rightVal
+					 case "/":
+							if rightVal==0{
+								return 0, fmt.Errorf("Деление на ноль!")
+							}
+							 result = leftVal / rightVal
+					 }
+					 
+					 values[curr] = result
+					 stack = stack[:len(stack)-1]
+			 } else {
+					 if curr.Right != nil {
+							 stack = append(stack, curr.Right)
+					 }
+					 if curr.Left != nil {
+							 stack = append(stack, curr.Left)
+					 }
+					 visited[curr] = true
+			 }
+	 }
+	 return values[node], nil
+ }
+ 
